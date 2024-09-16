a701abf391148e2d625fc1bc32975a9793b992ed
HansenAde <rimchie12@gmail.com>
2024-09-16 14:53:24 +0700

    Add files via upload

---
+++ app/java/MainMenu.kt
@@ -0,0 +1,21 @@
+package com.lanlords.vertimeter
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        val btnmulai = findViewById<Button>(R.id.btn_mulai)
+        btnmulai.setOnClickListener {
+            val intent = Intent(
+                this@MainMenuActivity,
+                MainActivity::class.java
+            )
+            startActivity(intent)
+        }
+    }
+}
